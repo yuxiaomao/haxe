56c823dff67fe7d48b4eb5bcdf06b94b4cca4982
Yuxiao Mao <yuxiao.mao@outlook.com>
2026-01-29 15:18:28 +0100

    [tests] add original runtime.Copy bug

--- tests/unit/src/unit/issues/Issue12538.hx
+++ tests/unit/src/unit/issues/Issue12538.hx
@@ -12,6 +12,32 @@ private enum E3 {
 	C(str:String);
 }
 
+@:structInit
+private class TVar {
+	public var id : Int;
+	public var type : TType;
+	public var kind : VarKind;
+}
+
+private enum VarKind {
+	Global;
+	Input;
+	Param;
+}
+
+private enum TType {
+	TVoid;
+	TArray( t : TType );
+	TSampler( dim : TexDimension );
+}
+
+enum TexDimension {
+	T1D;
+	T2D;
+	T3D;
+	TCube;
+}
+
 class Issue12538 extends Test {
 	function test() {
 		var map = new haxe.ds.ObjectMap();
@@ -27,5 +53,16 @@ class Issue12538 extends Test {
 		eq(null, map.get(b));
 		eq(2, map.get(c1));
 		eq(null, map.get(c2));
+	}
+
+	function testCopy() {
+		var v : TVar = {
+			id : 0,
+			type : TArray(TSampler(T3D)),
+			kind : Param,
+		};
+		var v2 = haxe.runtime.Copy.copy(v);
+		t(v != v2);
+		eq(v.kind, v2.kind);
 	}
 }
