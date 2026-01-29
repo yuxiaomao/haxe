57672bee2cd64a29d4efadf6d8e1e1ef38f5f981
Yuxiao Mao <yuxiao.mao@outlook.com>
2026-01-29 15:05:26 +0100

    [tests] add test ObjectMap with enum

---
+++ tests/unit/src/unit/issues/Issue12536.hx
@@ -0,0 +1,31 @@
+package unit.issues;
+
+private enum E1 {
+	A;
+}
+
+private enum E2 {
+	B;
+}
+
+private enum E3 {
+	C(str:String);
+}
+
+class Issue12536 extends Test {
+	function test() {
+		var map = new haxe.ds.ObjectMap();
+		var a : Dynamic = A;
+		var b : Dynamic = B;
+		var c1 : Dynamic = C("1");
+		var c2 : Dynamic = C("2");
+		map.set(a, 1);
+		eq(1, map.get(a));
+		eq(null, map.get(b));
+		map.set(c1, 2);
+		eq(1, map.get(a));
+		eq(null, map.get(b));
+		eq(2, map.get(c1));
+		eq(null, map.get(c2));
+	}
+}
