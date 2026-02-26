f6fc3e9fe615281aa3a9f9f367a4ac101c4aea39
Simon Krajewski <simon@haxe.org>
2026-02-26 07:31:31 +0100

    [coro] ExceptionHandler rework (#12675)
    * reset
    
    * delete DefaultExceptionHandler
    
    I'll move it to hxcoro instead because it's out-of-scope for std
    
    * update hxcoro branch
    
    * back to master

--- std/haxe/coro/context/ExceptionHandler.hx
+++ std/haxe/coro/context/ExceptionHandler.hx
@@ -1,8 +1,18 @@
 package haxe.coro.context;
 
 import haxe.Exception;
+import haxe.PosInfos;
 import haxe.coro.BaseContinuation;
+import haxe.coro.context.Context;
+import haxe.coro.context.IElement;
+import haxe.coro.context.Key;
 
+using StringTools;
+
+interface ISynchronousRun {
+	public var context(get, never):Context;
+	function complete():Void;
+}
 /**
 	An abstract context element that handles exception stack trace management for coroutines.
 	`BaseContinuation.startException` and `BaseContinuation.buildCallStack` delegate to this element.
@@ -11,6 +21,8 @@ import haxe.coro.BaseContinuation;
 **/
 abstract class ExceptionHandler implements IElement<ExceptionHandler> {
 	public static final key = new Key<ExceptionHandler>('ExceptionHandler');
+
+	abstract public function startSynchronousRun(context:Context, p:PosInfos):ISynchronousRun;
 
 	/**
 		Called when an exception is first encountered in a coroutine to process its stack trace.
@@ -26,97 +38,5 @@ abstract class ExceptionHandler implements IElement<ExceptionHandler> {
 
 	public function getKey() {
 		return key;
-	}
-}
-
-/**
-	The default `ExceptionHandler` implementation, which reconstructs the coroutine call stack
-	from the continuation chain and inserts it into the exception's stack trace.
-
-	`insertIndex` is stored in thread-local storage, making this implementation safe for
-	concurrent use across multiple coroutines running on different threads.
-**/
-class DefaultExceptionHandler extends ExceptionHandler {
-	#if debug
-	final insertIndex = new haxe.coro.Tls<Int>();
-	#end
-
-	public function new() {}
-
-	public function startException(cont:BaseContinuation<Any>, exception:Exception):Exception {
-		#if js
-		return exception;
-		#end
-		#if debug
-		@:privateAccess cont._hx_startedException = true;
-
-		var stack = [];
-		var skipping = 0;
-		var localInsertIndex = 0;
-		var stackItem = cont.getStackItem();
-
-		/*
-			Find first coro stack element
-		*/
-		var currentFrame:Null<haxe.coro.IStackFrame> = cont;
-		while (stackItem == null) {
-			currentFrame = currentFrame.callerFrame();
-			if (currentFrame == null) {
-				break;
-			}
-			stackItem = currentFrame.getStackItem();
-		}
-
-		switch (stackItem) {
-			case null:
-				return exception;
-			case FilePos(_, file, line, _):
-				for (index => item in exception.stack.asArray()) {
-					switch (item) {
-						case FilePos(_, file2, line2, _) if (skipping == 0 && file == file2 && line == line2):
-							stack.push(item);
-							skipping = 0;
-						// TODO: this is silly
-						case FilePos(Method("hxcoro.CoroRun", "run"), _) if (skipping == 1):
-							skipping = 2;
-						// this is a hack
-						case FilePos(Method(_, "invokeResume"), _) if (skipping == 0):
-							skipping = 1;
-							localInsertIndex = index;
-						case _:
-							if (skipping != 1) {
-								stack.push(item);
-							}
-					}
-				}
-			case _:
-				return exception;
-		}
-		exception.stack = stack;
-		insertIndex.value = localInsertIndex;
-		#end
-		return exception;
-	}
-
-	public function buildCallStack(cont:BaseContinuation<Any>):Void {
-		#if js
-		return;
-		#end
-		#if debug
-		if (@:privateAccess cont._hx_startedException) {
-			return;
-		}
-
-		final stackItem = cont.getStackItem();
-		if (stackItem != null) {
-			final idx = insertIndex.value;
-			if (idx != null) {
-				final stack = cont.error.stack.asArray();
-				stack.insert(idx, stackItem);
-				cont.error.stack = stack;
-				insertIndex.value = idx + 1;
-			}
-		}
-		#end
 	}
 }
