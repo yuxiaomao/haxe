2f37c312aec0244234ae33f98b5e55a201a0b4bc
Aidan Lee <aidan.lee63@gmail.com>
2026-05-04 14:20:01 +0100

    [cpp] Remove private specifier from Scratch extern to make it accessible
    Was there due to a dodgy copy and paste job

--- std/cpp/Scratch.hx
+++ std/cpp/Scratch.hx
@@ -3,7 +3,7 @@ package cpp;
 @:include("hx/thread/Scratch.hpp")
 @:semantics(value)
 @:cpp.ValueType({ namespace : [ "hx", "thread" ], flags : [ StackOnly ] })
-private extern class Scratch {
+extern class Scratch {
 	final view : cpp.marshal.View<cpp.UInt8>;
 
 	static function alloc(bytes:Int):Scratch;
